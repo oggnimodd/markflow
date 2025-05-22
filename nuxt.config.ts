6d320287e1488c39ae1dcc7430318b6fe92e2d40
oggnimodd <arnold.ds.dev@gmail.com>
2025-05-22 18:22:06 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  vite: { plugins: [tailwindcss()] },
+  modules: ["@nuxtjs/color-mode", "shadcn-nuxt"],
+  colorMode: {
+    classSuffix: "",
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
